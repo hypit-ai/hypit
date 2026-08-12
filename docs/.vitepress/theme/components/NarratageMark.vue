e45c33e85784579c22e50d558d8e2e773c9f4634
rpone <rponeawa@qq.com>
2026-08-12 21:29:40 +0800

    feat(docs): use SVG Narratage branding

---
+++ docs/.vitepress/theme/components/NarratageMark.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import markUrl from "../assets/narratage-logo/mark.svg";
+
+withDefaults(defineProps<{ class?: string; label?: string }>(), { label: "Narratage" });
+</script>
+
+<template>
+  <span :class="['narratage-mark', $props.class]" :style="{ '--narratage-mark-mask': `url(${markUrl})` }" role="img" :aria-label="label"></span>
+</template>
