a53a0db5b4574d25c423ff52a8c4113516f9ab5e
rpone <rponeawa@qq.com>
2026-08-19 17:39:45 +0800

    chore: remove generated element reference

--- packages/markup/src/index.ts
+++ packages/markup/src/index.ts
@@ -7,7 +7,6 @@ export {
   markupSurfaceHostFacetAbi,
 } from "./host-facet.js";
 export type { MarkupSurfaceHostFacetOptions } from "./host-facet.js";
-export { describeSurfaceVocabulary } from "./vocabulary.js";
 export {
   closeDocument,
   discoverMarkup,
