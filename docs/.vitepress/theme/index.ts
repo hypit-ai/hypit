4dc856a14af03c4e3f2758942cd541ac9959c895
rpone <rponeawa@qq.com>
2026-08-29 04:09:59 +0800

    Revert "fix documentation typography and responsive layout"
    This reverts commit 05a39ffe075bc591a87e132a7c4ed192aef9536b.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
 import Layout from "./Layout.vue";
-import "@fontsource-variable/hanken-grotesk";
 import "./style.css";
 
 export default {
