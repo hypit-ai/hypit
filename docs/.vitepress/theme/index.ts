4ecd5c74c68d225dcf55d55466c9d8d20b773a4a
rpone <rponeawa@qq.com>
2026-08-29 04:00:35 +0800

    fix documentation typography and responsive layout

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
 import Layout from "./Layout.vue";
+import "@fontsource-variable/hanken-grotesk";
 import "./style.css";
 
 export default {
